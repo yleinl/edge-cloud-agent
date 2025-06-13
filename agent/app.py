d7aae8f879c3a9ecb6747253f0a6ccbd0e20df80
abnerlyt <150734010+yleinl@users.noreply.github.com>
2025-06-13 22:49:10 +0200

    update offload strategy

--- agent/app.py
+++ agent/app.py
@@ -12,6 +12,7 @@ import time
 import requests
 import random
 from flask import current_app
+from agent.core.latency_ratio import LatencyRatioEstimator
 
 app = Flask(__name__)
 config_manager = None
@@ -22,6 +23,8 @@ response_log = defaultdict(deque)
 TIME_WINDOW = 60
 alpha = 0.3
 metrics_cache = None
+latency_tracker = defaultdict(lambda: LatencyRatioEstimator())
+
 
 def record_response_time(node_id, fn_name, duration):
     now = time.time()
@@ -30,92 +33,93 @@ def record_response_time(node_id, fn_name, duration):
 
     while response_log[key] and now - response_log[key][0][0] > TIME_WINDOW:
         response_log[key].popleft()
+    latencies = [rt for ts, rt in response_log[key]]
+    estimator = latency_tracker[key]
+    estimator.update(latencies)
+
+
+def get_offload_ratio(fn_name, arch):
+    key = (config_manager.self_node["id"], fn_name)
+    if arch == "decentralized":
+        key = (config_manager.self_node["id"], fn_name)
+    elif arch == "federated":
+        key = (config_manager.self_node["zone"], fn_name)
+    estimator = latency_tracker[key]
+    latencies = [rt for ts, rt in response_log[key]]
+
+    soft = config_manager.self_node["offload"].get("c_soft", 1.5)
+    hard = config_manager.self_node["offload"].get("c_hard", 3.0)
+    min_req = config_manager.self_node["offload"].get("min_requests", 10)
+
+    if len(latencies) < min_req:
+        return 0.0
+
+    estimator.update(latencies)
+    return estimator.compute_offload_ratio(soft, hard)
 
 
 def should_offload(configManager, fn_name):
-    self_node = configManager.self_node
     arch = configManager.get_architecture()
-    if not self_node["offload"].get("enabled", True):
-        return False
-
-    cpu_thresh = self_node["offload"].get("cpu_thresh", 0.9)
-    load_thresh = self_node["offload"].get("load_thresh", 3)
-
-    topo_map = configManager.topo_map
-    role = self_node.get("role")
-    zone = self_node.get("zone")
-    self_id = self_node.get("id")
-
-    # === Decentralized: focus on load status ===
-    if arch == "decentralized":
-        # cpu = psutil.cpu_percent(interval=0.1) / 100
-        # load0 = psutil.getloadavg()[0]
-        url = f"http://127.0.0.1:31113/metrics"
-        res = requests.post(url, json={"fn_name": fn_name}, timeout=60)
-
-        if res.status_code == 200:
-            data = res.json()
-            cpu = data["system_metrics"].get("cpu", 0)
-            load0 = data["system_metrics"].get("load0", 0)
-            return cpu > cpu_thresh and load0 > load_thresh
-        else:
-            return False
-
-    # === Federated：edge-controller pull zone status ===
-    # elif arch == "federated":
-    #     zone_members = [
-    #         node for node in topo_map.values()
-    #         if node.get("zone") == zone
-    #     ]
-    #     cpu_vals, load_vals = [], []
-    #     for node in zone_members:
-    #         try:
-    #             url = f"http://{node['address']}:31113/metrics"
-    #             res = requests.post(url, json={"fn_name": fn_name}, timeout=60)
-    #             if res.status_code == 200:
-    #                 data = res.json()
-    #                 cpu = data["system_metrics"].get("cpu")
-    #                 load0 = data["system_metrics"].get("load0")
-    #                 if cpu is not None:
-    #                     cpu_vals.append(cpu)
-    #                 if load0 is not None:
-    #                     load_vals.append(load0)
-    #         except Exception as e:
-    #             current_app.logger.warning(f"Failed to get /status from {node['id']}: {e}")
-    #
-    #     if not cpu_vals and not load_vals:
-    #         return False
-    elif arch == "federated":
-        if not metrics_cache:
-            return False
-
-        zone_nodes = [
-            node for node in topo_map.values() if node["zone"] == zone
-        ]
-
-        cpu_vals, load_vals = [], []
-        for node in zone_nodes:
-            m = metrics_cache.get_metrics(node["id"])
-            if m:
-                cpu_vals.append(m["cpu"])
-                load_vals.append(m["load0"])
-
-        if not cpu_vals or not load_vals:
-            return False
-
-        all_over_cpu = all(cpu > cpu_thresh for cpu in cpu_vals)
-        all_over_load = all(load > load_thresh for load in load_vals)
-        return all_over_cpu and all_over_load
-
-        # avg_cpu = sum(cpu_vals) / len(cpu_vals)
-        # avg_load = sum(load_vals) / len(load_vals)
-        #
-        # return avg_cpu > cpu_thresh and avg_load > load_thresh
-        # all_over_cpu = all(cpu > cpu_thresh for cpu in cpu_vals)
-        # all_over_load = all(load > load_thresh for load in load_vals)
-        # return all_over_cpu and all_over_load
-
-    return False
+    return random.random() < get_offload_ratio(fn_name, arch)
+# def should_offload(configManager, fn_name):
+#     self_node = configManager.self_node
+#     arch = configManager.get_architecture()
+#     if not self_node["offload"].get("enabled", True):
+#         return False
+#
+#     cpu_thresh = self_node["offload"].get("cpu_thresh", 0.9)
+#     load_thresh = self_node["offload"].get("load_thresh", 3)
+#
+#     topo_map = configManager.topo_map
+#     role = self_node.get("role")
+#     zone = self_node.get("zone")
+#     self_id = self_node.get("id")
+#
+#     # === Decentralized: focus on load status ===
+#     if arch == "decentralized":
+#         # cpu = psutil.cpu_percent(interval=0.1) / 100
+#         # load0 = psutil.getloadavg()[0]
+#         url = f"http://127.0.0.1:31113/metrics"
+#         res = requests.post(url, json={"fn_name": fn_name}, timeout=60)
+#
+#         if res.status_code == 200:
+#             data = res.json()
+#             cpu = data["system_metrics"].get("cpu", 0)
+#             load0 = data["system_metrics"].get("load0", 0)
+#             return cpu > cpu_thresh and load0 > load_thresh
+#         else:
+#             return False
+#     elif arch == "federated":
+#         if not metrics_cache:
+#             return False
+#
+#         zone_nodes = [
+#             node for node in topo_map.values() if node["zone"] == zone
+#         ]
+#
+#         cpu_vals, load_vals = [], []
+#         for node in zone_nodes:
+#             m = metrics_cache.get_metrics(node["id"])
+#             if m:
+#                 cpu_vals.append(m["cpu"])
+#                 load_vals.append(m["load0"])
+#
+#         if not cpu_vals or not load_vals:
+#             return False
+#
+#         all_over_cpu = all(cpu > cpu_thresh for cpu in cpu_vals)
+#         all_over_load = all(load > load_thresh for load in load_vals)
+#         return all_over_cpu and all_over_load
+#
+#         # avg_cpu = sum(cpu_vals) / len(cpu_vals)
+#         # avg_load = sum(load_vals) / len(load_vals)
+#         #
+#         # return avg_cpu > cpu_thresh and avg_load > load_thresh
+#         # all_over_cpu = all(cpu > cpu_thresh for cpu in cpu_vals)
+#         # all_over_load = all(load > load_thresh for load in load_vals)
+#         # return all_over_cpu and all_over_load
+#
+#     return False
 
 
 @app.route("/reload", methods=["POST"])
@@ -242,8 +246,6 @@ def entry():
                 }
             else:
                 result = invoke_local_faas(fn_name, payload)
-
-
         else:
             return jsonify({"error": f"Unsupported architecture: {arch}"}), 400
 
